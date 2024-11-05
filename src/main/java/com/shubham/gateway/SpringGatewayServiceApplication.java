8312c52e3f7aab7d85289d1fe517141fcefb86e8
shubhamsadare6626 <shubhamsadaress@gmail.com>
2024-11-05 12:43:40 +0530

    first commit-updated to github

---
+++ src/main/java/com/shubham/gateway/SpringGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/gateway/spring_gateway_service/SpringGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.gateway.spring_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
